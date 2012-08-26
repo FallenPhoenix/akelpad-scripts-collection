7dfadba85490346338c1c6a365b4b8337a598b9c
F. Phoenix <fphoenix@email.com>
2012-08-27 00:08:00 +0400

    ↑ ForceNewInstance.js v1.2

--- Instructor/ForceNewInstance.js
+++ Instructor/ForceNewInstance.js
@@ -1,29 +1,46 @@
 // http://akelpad.sourceforge.net/forum/viewtopic.php?p=4222#4222
-// Version v1.1
+// Version v1.2
 //
 //
 //// Force create new instance.
 
 var hMainWnd=AkelPad.GetMainWnd();
-var oSys=AkelPad.SystemFunction();
-var lpPluginData;
-var hMenuMain=0;
-var hNewMainWnd;
-var dwState;
 
-if (hMainWnd)
+ForceNewInstance(hMainWnd, true);
+
+function ForceNewInstance(hMainWnd, bNoSessions)
 {
-  if (hMenuMain=AkelPad.SendMessage(hMainWnd, 1222 /*AKD_GETMAININFO*/, 21 /*MI_MENUMAIN*/, 0))
+  var hNewMainWnd=0;
+  var lpPluginFunction;
+  var bAutoLoad=false;
+
+  //Turn off Sessions plugin autoload
+  if (bNoSessions)
   {
-    dwState=oSys.Call("user32::GetMenuState", hMenuMain, 4256 /*IDM_OPTIONS_SINGLEOPEN_PROGRAM*/, 0 /*MF_BYCOMMAND*/);
-  
-    if (dwState & 0x8 /*MF_CHECKED*/)
+    if (lpPluginFunction=AkelPad.SendMessage(hMainWnd, 1331 /*AKD_DLLFINDW*/, AkelPad.MemStrPtr("Sessions::Main"), 0))
     {
-      AkelPad.Command(4256 /*IDM_OPTIONS_SINGLEOPEN_PROGRAM*/);
-      hNewMainWnd=AkelPad.Command(4102 /*IDM_FILE_CREATENEW*/);
-      AkelPad.Command(4256 /*IDM_OPTIONS_SINGLEOPEN_PROGRAM*/);
-      AkelPad.SendMessage(hNewMainWnd, 273 /*WM_COMMAND*/, 4256 /*IDM_OPTIONS_SINGLEOPEN_PROGRAM*/, 0);
+      if (bAutoLoad=AkelPad.MemRead(lpPluginFunction + (_X64?812:800) /*offsetof(PLUGINFUNCTION, bAutoLoad)*/, 3 /*DT_DWORD*/))
+      {
+        AkelPad.MemCopy(lpPluginFunction + (_X64?812:800) /*offsetof(PLUGINFUNCTION, bAutoLoad)*/, false, 3 /*DT_DWORD*/);
+        AkelPad.SendMessage(hMainWnd, 1336 /*AKD_DLLSAVE*/, 0x1 /*DLLSF_NOW*/, 0);
+      }
     }
-    else AkelPad.Command(4102 /*IDM_FILE_CREATENEW*/);
   }
+
+  if (AkelPad.SendMessage(hMainWnd, 1222 /*AKD_GETMAININFO*/, 153 /*MI_SINGLEOPENPROGRAM*/, 0))
+  {
+    AkelPad.Command(4256 /*IDM_OPTIONS_SINGLEOPEN_PROGRAM*/);
+    hNewMainWnd=AkelPad.Command(4102 /*IDM_FILE_CREATENEW*/);
+    AkelPad.Command(4256 /*IDM_OPTIONS_SINGLEOPEN_PROGRAM*/);
+    AkelPad.SendMessage(hNewMainWnd, 273 /*WM_COMMAND*/, 4256 /*IDM_OPTIONS_SINGLEOPEN_PROGRAM*/, 0);
+  }
+  else hNewMainWnd=AkelPad.Command(4102 /*IDM_FILE_CREATENEW*/);
+
+  //Turn on Sessions plugin autoload
+  if (bNoSessions && bAutoLoad)
+  {
+    AkelPad.MemCopy(lpPluginFunction + (_X64?812:800) /*offsetof(PLUGINFUNCTION, bAutoLoad)*/, bAutoLoad, 3 /*DT_DWORD*/);
+    AkelPad.SendMessage(hMainWnd, 1336 /*AKD_DLLSAVE*/, 0x1 /*DLLSF_NOW*/, 0);
+  }
+  return hNewMainWnd;
 }
