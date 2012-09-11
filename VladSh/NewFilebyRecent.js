ad95ec5b0fd389813ad2643f73333ff27c8b2aef
F. Phoenix <fphoenix@email.com>
2012-09-11 15:18:00 +0400

    ↑ NewFilebyRecent.js v2.8

--- VladSh/NewFilebyRecent.js
+++ VladSh/NewFilebyRecent.js
@@ -1,15 +1,15 @@
 ///Creation of a new file on the basis of the contained allocated file in the list of the fresh
 ///Универсальное создание нового файла на основе имеющегося в окне редактирования или в списке свежих
 // http://akelpad.sourceforge.net/forum/viewtopic.php?p=1505#1505
-// Version: 2.7 (2012.09.02)
+// Version: 2.8 (2012.09.11)
 // 
 // -"Новый на основе выбранного" Call("Scripts::Main", 1, "NewFilebyRecent.js", `"%f"`)  Icon("%a\AkelFiles\Plugs\ToolBar.dll", 1) - из меню свежих файлов (параметр `"%f"` обязателен!)
 // -"Новый на основе текущего" Call("Scripts::Main", 1, "NewFilebyRecent.js")  Icon("%a\AkelFiles\Plugs\ToolBar.dll", 1) - меню вкладок, окна редактирования или тулбара
 
 var fileName = "";
-var pContent = "";
 var ext = "";
 var fso = new ActiveXObject("Scripting.FileSystemObject");
+var pContent = "";
 
 if (WScript.Arguments.length) {
 	if (WScript.Arguments(0) != AkelPad.GetEditFile(0))
@@ -22,29 +22,21 @@ if (fileName) {
 		//Считывание содержимого файла
 		pContent = AkelPad.ReadFile(fileName)
 	}
-	
 	ext = fso.GetExtensionName(fileName);
-	AkelPad.Command(4101 /*IDM_FILE_NEW*/);		//создание (закладки) нового файла
 }
 else {
-	fileName = AkelPad.GetEditFile(0);			//стартовали из меню вкладок или окна редактирования
-	
+	//Стартовали из меню вкладок или окна редактирования
 	//Пытаемся сначала взять выделенный текст и на основе его создать док, иначе создаём на основе всего текста файла текущей вкладки
 	pContent = AkelPad.GetSelText() || AkelPad.GetTextRange(0, -1);
-	
-	if (AkelPad.Include("CommonFunctions.js")) {
-		ext = getActiveSyntax(0);
-		CreateByFile(0);		//создаём со всеми параметрами исходного файла
-	}
-	else
-		AkelPad.Command(4101 /*IDM_FILE_NEW*/);
 }
+
+if (AkelPad.Include("CommonFunctions.js"))
+	CreateByFile(0, ext);		//создаём со всеми параметрами исходного файла
+else
+	AkelPad.Command(4101 /*IDM_FILE_NEW*/);
 
 //Запись содержимого в окно редактирования Akel'а
 AkelPad.ReplaceSel(pContent);
-
-if (ext && AkelPad.IsPluginRunning("Coder::HighLight") == true)
-	AkelPad.Call("Coder::Settings", 1, ext);
 
 //Установка курсора в начало нового файла
 AkelPad.SetSel(0, 0);
