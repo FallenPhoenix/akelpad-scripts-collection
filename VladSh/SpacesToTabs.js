e83cdc19f2dbe8ad6eb44814a2814763fd2d0c07
F. Phoenix <fphoenix@email.com>
2012-09-12 22:22:00 +0400

    ↑ SpacesToTabs.js v3.1

--- VladSh/SpacesToTabs.js
+++ VladSh/SpacesToTabs.js
@@ -1,69 +1,104 @@
-/// Convert space to tabulation
-// Arguments:
-//    1 - definition of number of spaces in block: "1" - from program settings; "2" - by default (2 spases), "3" - InputBox
-//    2 - what to do with the residual single-spaces: "0" (or without this argument) - remain unchanged (use for processing scripts); "-1" - remove it; "1" - replace on a Tab character
+///Convert space to tabulation
 // http://akelpad.sourceforge.net/forum/viewtopic.php?p=7947#7947
-// Version: 2.0 (2011.02.03)
+// Version: 3.1 (2012.09.12) - добален функционал + некоторые идеи вз€ты из соответствующего скрипта от F.Phoenix (2012-08-18a)
+//
+// Arguments:
+//		lead:
+//			Х 0 - processing of all text (used for vertical text blocks)
+//			Х [1] - processing of lead spaces only (used for scripts)
+//		bSpaces - definition of count of spaces in block:
+//			Х [0] - from program settings
+//			Х -1 - InputBox
+//			Х > 0 - count of spaces to be replaced by a tab
+//		oActn - what to do with other (the residual) single-spaces:
+//			Х [0] (or without this argument) - do nothing, to limit the processing of block spaces only (used for scripts)
+//			Х 1 - replace single-spaces
+//			Х -1 - remove single-spaces (can also be used for scripts)
+//		sel (0/1) - leave the result text selected; without this argument it defined automatically
+//
+//	Example:
+//		-"Convert space to tabulation" Call("Scripts::Main", 1, "SpacesToTabs.js") - script process by default options: processing of lead spaces only, definition of count of spaces in block from program settings, residual single-spaces will not be processed
 
-if (AkelPad.GetMainWnd())
-{
+var constsDefault = {
+	msgp_SpasesInTab: "Enter the number of spaces in a tab character:",
+	msgp_InputNaN: "Input non-numeric value, the operation is canceled.",
+	msgp_ArgumentNaN: "Arguments passed a non-numeric value, the operation is canceled."
+};
+
+if (AkelPad.GetMainWnd()) {
 	var hWndEdit = AkelPad.GetEditWnd();
-	if (WScript.Arguments.length == 0)
-	{
-		AkelPad.MessageBox(hWndEdit, "јргументы отсутствуют; операци€ остановлена...", WScript.ScriptName, 48 /*MB_ICONEXCLAMATION*/);
-		WScript.Quit();
-	}
 	
-	//Getting selected text
 	var bTextSelected = true;
 	var pSelText = AkelPad.GetSelText();
-	if (!pSelText)
-	{
+	if (!pSelText) {
 		pSelText = AkelPad.GetTextRange(0, -1);
 		bTextSelected = false;
 	}
 	if (!pSelText) WScript.Quit();
 	
-	var nTabStopDefault = 2;
+	var sReplaced = "\t";		// замен€ющий текст
 	var nTabStop;
-	switch (WScript.Arguments(0))
-	{
-		case "1":		//from program settings
-			var nTabStop = AkelPad.SendMessage(hWndEdit, 3239 /*AEM_GETTABSTOP*/, 0, 0);
-			break
-		case "3":		//InputBox
-			nTabStop = AkelPad.InputBox(hWndEdit, WScript.ScriptName, "¬ведите количество пробелов в одном символе табул€ции", nTabStopDefault);
-			
-			if (isNaN(nTabStop))		//not isNumber
-			{
-				if (nTabStop) AkelPad.MessageBox(hWndEdit, "¬ведено нечисловое значение, операци€ отменена...", WScript.ScriptName, 48 /*MB_ICONEXCLAMATION*/);			//press Cancel
-				WScript.Quit();
-			}
-			break
-		default:		//by default
-			nTabStop = nTabStopDefault;
+	var nBSAction = AkelPad.GetArgValue("bSpaces", 0);
+	if (nBSAction <= 0) {
+		//0 - from program settings
+		var nTabStop = AkelPad.SendMessage(hWndEdit, 3239 /*AEM_GETTABSTOP*/, 0, 0);
+		// InputBox
+		if (nBSAction == -1) {
+			if (!(nTabStop = AkelPad.InputBox(hWndEdit, WScript.ScriptName, getConst("msgp_SpasesInTab"), nTabStop))) WScript.Quit();
+			checkTabStopIsNaN(hWndEdit, nTabStop, "msgp_InputNaN");
+		}
+	}
+	else {
+		checkTabStopIsNaN(hWndEdit, nBSAction, "msgp_ArgumentNaN");
+		nTabStop = nBSAction;
 	}
 	
-	//Running the code change (common part)
-	for (var nSpaceCount = nTabStop; nSpaceCount > 1; --nSpaceCount)
-	{
-		var exprSpases = new RegExp(' {' + nSpaceCount + '}', "g");
-		pSelText = pSelText.replace(exprSpases, "\t");
+	// ќбработка блоков из определЄнного количества пробелов
+	if (AkelPad.GetArgValue("lead", 1)) {
+		// обработка только лидирующиех пробелов
+		pSelText = pSelText.replace(/^\s+/mg, replacer);
 	}
-	
-	if (WScript.Arguments.length > 1)
-	{
-		switch (WScript.Arguments(1))
-		{
-			case "1":		//replace on a Tab character
-				pSelText = pSelText.replace(/ /g, "\t");
-				break
-			case "-1":		//remove it
-				pSelText = pSelText.replace(/ /g, "");
-				break
+	else {
+		// обработка пробелов всего текста (дл€ вертикальных блоков)
+		pSelText = replacer(pSelText);
+		
+		// ќбработка оставшихс€ пробелов
+		var nOSAction = AkelPad.GetArgValue("oActn", 0);
+		if (nOSAction != 0) {
+			//replace residual space on a Tab character or remove it
+			sReplaced = (nOSAction == 1) ? "\t" : "";
+			pSelText = pSelText.replace(/ /g, sReplaced);
 		}
 	}
 	
 	if (!bTextSelected) AkelPad.SetSel(0, -1);
-	AkelPad.ReplaceSel(pSelText);
+	var bTextSelected = AkelPad.GetArgValue("sel", bTextSelected);
+	AkelPad.ReplaceSel(pSelText, bTextSelected);
+}
+
+function checkTabStopIsNaN(hWndEdit, nTabStop, pPlaceActionMsg) {
+	if (isNaN(nTabStop))	{
+		AkelPad.MessageBox(hWndEdit, getConst(pPlaceActionMsg), WScript.ScriptName, 48 /*MB_ICONEXCLAMATION*/);			//press Cancel
+		WScript.Quit();
+	}
+}
+
+function replacer(str) {
+	for (var nSpaceCount = nTabStop; nSpaceCount > 1; --nSpaceCount) {
+		var exprSpases = new RegExp(' {' + nSpaceCount + '}', "g");
+		str = str.replace(exprSpases, sReplaced);
+	}
+	return str;
+}
+
+function getConst(constName) {
+	var sResult;
+	var oSet = AkelPad.ScriptSettings();
+	if (oSet.Begin(WScript.ScriptBaseName, 0x1 /*POB_READ*/)) {
+		sResult = oSet.Read(AkelPad.GetLangId(0 /*LANGID_FULL*/) + "_" + constName, 3 /*PO_STRING*/);
+		oSet.End();
+	}
+	else
+		sResult = constsDefault[constName];
+	return sResult;
 }
