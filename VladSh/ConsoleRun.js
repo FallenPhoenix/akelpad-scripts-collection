c6018aea00ce102ff6567eabe21b5256ba3c12f6
F. Phoenix <fphoenix@email.com>
2012-09-12 23:17:00 +0400

    ↑ ConsoleRun.js v2.7

--- VladSh/ConsoleRun.js
+++ VladSh/ConsoleRun.js
@@ -1,18 +1,14 @@
 ///Выполняет команду с помощью cmd (аналог WshShell.Run), с выводом результата выполнения в консоли Log-плагина
 // http://akelpad.sourceforge.net/forum/viewtopic.php?p=1212#1212
-// Version: 2.6 (2012.02.23)
+// Version: 2.7 (2012.09.12)
 // 
-// -"ipconfig" Call("Scripts::Main", 1, "ConsoleRun.js", `"ipconfig"`)
+// -"ipconfig" Call("Scripts::Main", 1, "ConsoleRun.js", `-cmd="ipconfig"`)
 
-var pCommand;
-
-if (WScript.Arguments.length == 1)
-	pCommand = WScript.Arguments(0);
-else
-{
+var pCommand = AkelPad.GetArgValue("cmd", "");
+if (!pCommand) {
 	if (! AkelPad.Include("ShowMenuEx.js")) WScript.Quit();
 	pCommand = getSelectedMenuItem(POS_CURSOR, "0", 0);
+	if (!pCommand) WScript.Quit();
 }
 
-if (pCommand)
-	AkelPad.Call("Log::Output", 1, pCommand, "", "", "", -1, -1);
+AkelPad.Call("Log::Output", 1, pCommand, "", "", "", -1, -1);
