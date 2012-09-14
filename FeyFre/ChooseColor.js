0e17d0f2b6883c2d818cedc8606520e80776daee
F. Phoenix <fphoenix@email.com>
2012-09-14 18:09:00 +0400

    ↑ ChooseColor.js v1.7.1

--- FeyFre/ChooseColor.js
+++ FeyFre/ChooseColor.js
@@ -1,65 +1,77 @@
 ﻿// Choose & insert/change color in RGB-format by standard color selection dialog
 // supported short format of standard colors
 // http://akelpad.sourceforge.net/forum/viewtopic.php?p=8611#8611
-// Version: 1.6.1 (2011.02.16)	se7h (autoselect color under caret)
-// Version: 1.5 (2011.02.01)	VladSh (change; short format)
-// Version: 1.4 (2011.01.26)	VladSh (изменение выделенного цвета)
-// Version: 1.3 (2011.01.18)	© FeyFre
+// http://staynormal.org.ua/akelpad/scripts/ChooseColor.js
+// Version: 1.7.1 (2012.09.14)   VladSh (warning fixes by Lint)
+// Version: 1.7   (2012.09.13)   VladSh (bug fixes, added/changed variation of formats of input/output data)
+// Version: 1.6.1 (2011.02.16)   se7h (autoselect color under caret)
+// Version: 1.5   (2011.02.01)   VladSh (change; short format)
+// Version: 1.4   (2011.01.26)   VladSh (изменение выделенного цвета)
+// Version: 1.3   (2011.01.18)   © FeyFre
 //
+// Arguments:
+//    default - значения цветов, которые будут отображаться при открытии (при отсутствии выделенного текста); см. пример ниже
+//    lcase ([0 | без параметра] / 1) - возможность вывода результата в нижнем регистре
+//    place - куда помещать выводимый результат:
+//       • [0 | без параметра] - в окно редактирования (замена выделенного текста)
+//       • 1 - в буфер обмена
+//       • 2 - в поле диалога (InputBox)
+//
+// Examples:
 // -"Insert color..." Call("Scripts::Main", 1, "ChooseColor.js") Icon("%a\AkelFiles\Plugs\Toolbar.dll", 30)
-// -"Вставка цвета..." Call("Scripts::Main", 1, "ChooseColor.js", `"127" "127" "127"`) Icon("%a\AkelFiles\Plugs\Toolbar.dll", 30)		//initial color values from se7h
+// -"Вставка цвета..." Call("Scripts::Main", 1, "ChooseColor.js", `-default="127 127 127"`) Icon("%a\AkelFiles\Plugs\Toolbar.dll", 30)      //initial color values from se7h
 
 //color values by default
 var nRGB = [255 /*RED*/, 0 /*GREEN*/, 0 /*BLUE*/];
 
-var pSelText = "";
-var hWndEdit=AkelPad.GetEditWnd();
-var nSelStart;
-var nSelEnd;
-var nWordBegin;
-var nWordEnd;
-
-if(hWndEdit)
+var nPlace = AkelPad.GetArgValue("place", 0);
+var hWndEdit = AkelPad.GetEditWnd();
+if (hWndEdit)
 {
-	nSelStart=AkelPad.GetSelStart();
-	nSelEnd=AkelPad.GetSelEnd();
-	if(nSelStart == nSelEnd)
-	{
-		//Select word under caret
-		nWordBegin=AkelPad.SendMessage(hWndEdit, 1100 /*EM_FINDWORDBREAK*/, 0 /*WB_LEFT*/, nSelStart);
-		nWordEnd=AkelPad.SendMessage(hWndEdit, 1100 /*EM_FINDWORDBREAK*/, 7 /*WB_RIGHTBREAK*/, nWordBegin);
-		if (nWordEnd > nSelStart)
-		{
-			pSelText=AkelPad.GetTextRange(nWordBegin, nWordEnd);
-			if(AkelPad.GetTextRange(nWordBegin-1, nWordBegin) == "#")
-				nWordBegin = nWordBegin - 1;
-			AkelPad.SetSel(nWordBegin, nWordEnd);
-		}
-	}
-	else pSelText=AkelPad.GetTextRange(nSelStart, nSelEnd);
+   var crSel = [];
+   var pSelText = AkelPad.GetSelText();
+   if (!pSelText) {
+      crSel = getWordCaretInfo(hWndEdit);
+      if (crSel) pSelText = AkelPad.GetTextRange(crSel.min, crSel.max);
+   }
+   else {
+      crSel.min = AkelPad.GetSelStart();
+      crSel.max = AkelPad.GetSelEnd();
+   }
+   if (pSelText) {
+      // проверка возможного НЕзахвата решётки
+      if (AkelPad.GetTextRange(crSel.min-1, crSel.min) == "#")
+         crSel.min = crSel.min - 1;
+      // корректировка избыточной длины (при выделении вручную)
+      var lRgbMax = 7;
+      if ((crSel.max - crSel.min) > lRgbMax)
+         crSel.max = crSel.min + lRgbMax;
+      AkelPad.SetSel(crSel.min, crSel.max);
+      pSelText = AkelPad.GetSelText();
+   }
+}
+else {
+   // если окна редактирования нет, то выводим результат в InputBox, игнорируя переданный параметр
+   if (nPlace === 0) nPlace = 2;
 }
 
-if(!pSelText)
-{
-	if(WScript.Arguments.length >= 1)
-	{
-		nRGB[0] = parseInt(WScript.Arguments(0));
-		if(WScript.Arguments.length >= 2)
-		{
-			nRGB[1] = parseInt(WScript.Arguments(1));
-			if(WScript.Arguments.length > 2)
-				nRGB[2] = parseInt(WScript.Arguments(2));
-		}
-	}
+if (!pSelText) {
+   var dRGB = AkelPad.GetArgValue("default", "");
+   if (dRGB) {
+      dRGB = dRGB.split(" ");
+      for (var i = 0; i < dRGB.length; i++)
+         nRGB[i] = parseInt(dRGB[i]);
+      dRGB = null;
+   }
 }
 else
-	HexToRGB(pSelText, nRGB);
+   nRGB = HexToRGB(pSelText);
 
 var oFunc = AkelPad.SystemFunction();
 var /*CHOOSECOLOR*/ ccs = AkelPad.MemAlloc((_X64?8:4) * 9);
 var /*COLORREF[16]*/ lprgbcustcol = AkelPad.MemAlloc(4 * 16);
-for(var i = 0; i < 16; i++)
-	AkelPad.MemCopy(lprgbcustcol + i * 4, 0x0FFFFFF, 3 /*DT_DWORD*/);
+for (i = 0; i < 16; i++)
+   AkelPad.MemCopy(lprgbcustcol + i * 4, 0x0FFFFFF, 3 /*DT_DWORD*/);
 
 //!CHOOSECOLOR.lStructSize
 AkelPad.MemCopy(ccs + (_X64?0:0), (_X64?8:4) * 9, 3 /*DT_DWORD*/);
@@ -80,49 +92,62 @@ AkelPad.MemCopy(ccs + (_X64?56:28), 0, 2 /*DT_QWORD*/);
 //!CHOOSECOLOR.lpTemplateName
 AkelPad.MemCopy(ccs + (_X64?64:32), 0, 2 /*DT_QWORD*/);
 
-var res = oFunc.Call("comdlg32::ChooseColor"+_TCHAR, ccs);
-if(res)
-{
-	// COLORREF in format 0x00BBGGRR
-	var xColor = AkelPad.MemRead(ccs + (_X64?24:12), 3 /*DT_DWORD*/);
+if (oFunc.Call("comdlg32::ChooseColor" + _TCHAR, ccs)) {
+   // COLORREF in format 0x00BBGGRR
+   var xColor = AkelPad.MemRead(ccs + (_X64?24:12), 3 /*DT_DWORD*/);
 
-	var hColor = RGBToHex(xColor);
+   var hColor = RGBToHex(xColor);
+   if (AkelPad.GetArgValue("lcase", 0))
+      hColor = hColor.toLowerCase();
 
-	AkelPad.ReplaceSel(hColor, true);
-	//AkelPad.InputBox(AkelPad.GetMainWnd(), "AkelPad -> " + WScript.ScriptName, "Цвет в RGB:", hColor);
+   switch (nPlace) {
+      case 1:
+         AkelPad.SetClipboardText(hColor);
+         break;
+      case 2:
+         AkelPad.InputBox(AkelPad.GetMainWnd(), WScript.ScriptName, "Color in RGB:", hColor);
+         break;
+      default:
+         AkelPad.ReplaceSel(hColor, true);
+         break;
+   }
 }
 
 AkelPad.MemFree(ccs);
 AkelPad.MemFree(lprgbcustcol);
 
 
-function HexToRGB(hColor, xColor)
-{
-	hColor = hColor.replace("#", "");
-
-	var nStart = ((hColor.length == 6) + 1);
-	var nOffset = 0;
-	var vColor = "";
-	for(var n = 0; n <= 2; n++)
-	{
-		vColor = hColor.substring(0 + nOffset, nStart + nOffset);
-		if(vColor.length == 1) vColor += vColor;
-		xColor[n] = (parseInt(vColor, 16)) || 0;
-		nOffset += nStart;
-	}
+function HexToRGB(hColor) {
+   var dec = parseInt(hColor.charAt(0) == '#' ? hColor.slice(1) : hColor, 16);
+   return [dec >> 16, dec >> 8 & 255, dec & 255];
 }
 
-function RGBToHex(xColor)
-{
-	var hex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
-	var hRGB = ['', '', ''];
-	for(var n = 0; n <= 2; n++)
-	{
-		for(var i = 0; i < 2; i++)
-		{
-			hRGB[n] = hex[xColor%16] + hRGB[n];
-			xColor = Math.floor(xColor / 16);
-		}
-	}
-	return "#" + hRGB.join("");
+function RGBToHex(xColor) {
+// переделать бы как-то на алгоритм от 2: http://jsperf.com/rgbtohex/5 ...
+   var hex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+   var hRGB = ['', '', ''];
+   for (var n = 0; n <= 2; n++) {
+      for (var i = 0; i < 2; i++) {
+         hRGB[n] = hex[xColor%16] + hRGB[n];
+         xColor = Math.floor(xColor / 16);
+      }
+   }
+   return "#" + hRGB.join("");
+}
+
+//Select word under caret
+function getWordCaretInfo(hWndEdit) {
+   if (hWndEdit) {
+      var nCaretPos = AkelPad.GetSelStart();
+      var crInfo = [];
+      crInfo.min = AkelPad.SendMessage(hWndEdit, 1100 /*EM_FINDWORDBREAK */, 0/*WB_LEFT*/, nCaretPos);
+      crInfo.max = AkelPad.SendMessage(hWndEdit, 1100 /*EM_FINDWORDBREAK */, 7/*WB_RIGHTBREAK*/, crInfo.min);
+      //! For case when caret located on word start position i.e. "prev-word |word-to-copy"
+      if (crInfo.max < nCaretPos) {
+         crInfo.min = AkelPad.SendMessage(hWndEdit, 1100/*EM_FINDWORDBREAK*/, 0/*WB_LEFT*/, nCaretPos + 1);
+         crInfo.max = AkelPad.SendMessage(hWndEdit, 1100/*EM_FINDWORDBREAK*/, 7/*WB_RIGHTBREAK*/, crInfo.min);
+      }
+      if (crInfo.max >= nCaretPos)
+         return crInfo;
+   }
 }
