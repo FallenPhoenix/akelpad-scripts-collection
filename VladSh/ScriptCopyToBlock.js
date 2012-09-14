d33d9b7dea7ef6ebc433d69d652fb1cbb4332238
F. Phoenix <fphoenix@email.com>
2012-09-14 11:22:00 +0400

    ↑ ScriptCopyToBlock.js v1.2

--- VladSh/ScriptCopyToBlock.js
+++ VladSh/ScriptCopyToBlock.js
@@ -1,18 +1,28 @@
 ///Копирует содержимое файла в определённую структуру [] для выкладки на форум
 // http://akelpad.sourceforge.net/forum/viewtopic.php?p=13573#13573
-// Version: 1.1 (2011.11.22)
+// Version: 1.2 (2012.09.14)
 // 
+// Аргументы:
+// 	-file="%f" - ссылка на файл
+// 	-block - наименование тэга контейнера
+// 	-bold ([0]/1) - "жирное" наименование скрипта
+// 
+// Примеры:
 // -"Содержимое файла в [more]..."  Call("Scripts::Main", 1, "ScriptCopyToBlock.js", `-file="%f"`)
 // -"Содержимое файла в [spoiler]..."  Call("Scripts::Main", 1, "ScriptCopyToBlock.js", `-file="%f" -block="spoiler"`)
 
 var pFileName = GetFileName(AkelPad.GetArgValue("file", ""));
 var pBlockName = AkelPad.GetArgValue("block", "more");
-var pScript = '[b][' + pBlockName + '="' + pFileName + '"][/b]\r[code]' + AkelPad.GetTextRange(0, -1) + '[/code][/' + pBlockName + ']';
+var bOpen = ""; var bClose = "";
+var b = AkelPad.GetArgValue("bold", 0);
+if (b) {
+	bOpen = "[b]";
+	bClose = "[/b]";
+}
+var pScript = bOpen + '[' + pBlockName + '="' + pFileName + '"]' + bClose + '\r[code]' + AkelPad.GetTextRange(0, -1) + '[/code][/' + pBlockName + ']';
 if (pScript)
 	AkelPad.SetClipboardText(pScript);
 
-
-function GetFileName(pFile)
-{
+function GetFileName(pFile) {
 	return pFile.slice(pFile.lastIndexOf('\\') + 1);
 }
