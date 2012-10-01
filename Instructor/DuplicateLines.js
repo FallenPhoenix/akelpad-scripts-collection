a07180d4b418a335528122061fd33d41966ad9d1
F. Phoenix <fphoenix@email.com>
2012-10-01 18:59:00 +0400

    ↑ DuplicateLines.js v1.1

--- Instructor/DuplicateLines.js
+++ Instructor/DuplicateLines.js
@@ -1,18 +1,21 @@
 // http://akelpad.sourceforge.net/forum/viewtopic.php?p=6504#6504
-// Version v1.0
+// Version v1.1
 //
 //
 //// Duplicate selected lines.
 
-var hMainWnd=AkelPad.GetMainWnd();
+var hWndEdit=AkelPad.GetEditWnd();
 var pSelText;
 var pLinesArray;
 var pResultArray=new Array();
 var nLinesIndex;
 var nResultIndex=0;
 
-if (hMainWnd)
+if (hWndEdit)
 {
+  if (AkelPad.GetSelStart() == AkelPad.GetSelEnd())
+    SelectCaretLine(hWndEdit);
+
   if (pSelText=AkelPad.GetSelText())
   {
     if (pLinesArray=pSelText.split("\r"))
@@ -23,7 +26,21 @@ if (hMainWnd)
         pResultArray[nResultIndex++]=pLinesArray[nLinesIndex];
       }
       pSelText=pResultArray.join("\r");
-      AkelPad.ReplaceSel(pSelText, true);
     }
+    AkelPad.ReplaceSel(pSelText, true);
   }
+}
+
+function SelectCaretLine(hWnd)
+{
+  var nCaretOffset=AkelPad.GetSelStart();
+  var nCaretLine;
+  var nLineStart;
+  var nLineEnd;
+
+  nCaretLine=AkelPad.SendMessage(hWnd, 1078 /*EM_EXLINEFROMCHAR*/, 0, nCaretOffset);
+  nLineStart=AkelPad.SendMessage(hWnd, 187 /*EM_LINEINDEX*/, nCaretLine, 0);
+  nLineEnd=nLineStart + AkelPad.SendMessage(hWnd, 193 /*EM_LINELENGTH*/, nCaretOffset, 0);
+
+  AkelPad.SetSel(nLineStart, nLineEnd);
 }
