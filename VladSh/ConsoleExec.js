beee2d2c7b10e20a3784124de677b66289457a46
F. Phoenix <fphoenix@email.com>
2012-12-18 16:09:00 +0400

    ↑ ConsoleExec.js v2.11

--- VladSh/ConsoleExec.js
+++ VladSh/ConsoleExec.js
@@ -1,14 +1,19 @@
 ///Выполняет команду методом WshShell.Exec; используется для запуска некоторых приложений, аналог команды "Пуск" -> "Выполнить"
 // http://akelpad.sourceforge.net/forum/viewtopic.php?p=1212#1212
-// Version: 2.10 (2012.09.12)
+// Version: 2.11 (2012.12.18)
 // 
-// -"Калькулятор" Call("Scripts::Main", 1, "ConsoleExec.js", `-cmd="calc"`)
-// -"Выбор программы для запуска..." Call("Scripts::Main", 1, "ConsoleExec.js", `-ignore1menu=1`)	- пример задания игнорирования вывода меню, если в param-файле одна запись
+// Параметры:
+// 	cmd - имя команды; если указано, то меню выводиться не будет, а выполнится указанная команда;
+// 	sep - разделитель для разбора идентификаторов и надписей в param-файле (подробнее см. параметр pParsepProcSep в ShowMenuEx.js)
+// 
+// Примеры:
+// 	-"Калькулятор" Call("Scripts::Main", 1, "ConsoleExec.js", `-cmd="calc"`)
+// 	-"Выбор программы для запуска..." Call("Scripts::Main", 1, "ConsoleExec.js", `-sep="=" -ignore1menu=1`)	- пример задания игнорирования вывода меню, если в param-файле одна запись
 
 var pCommand = AkelPad.GetArgValue("cmd", "");
 if (!pCommand) {
-	if (! AkelPad.Include("ShowMenuEx.js")) WScript.Quit();
-	pCommand = getSelectedMenuItem(POS_CURSOR, "", 0);
+	if (!AkelPad.Include("ShowMenuEx.js")) WScript.Quit();
+	pCommand = getSelectedMenuItem(POS_CURSOR, AkelPad.GetArgValue("sep", ""), 0);
 	if (!pCommand) WScript.Quit();
 }
 
