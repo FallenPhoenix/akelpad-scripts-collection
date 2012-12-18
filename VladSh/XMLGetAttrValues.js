f6b0264dee9fe7d6243e49e93ea7e0105282da49
F. Phoenix <fphoenix@email.com>
2012-12-18 15:53:00 +0400

    ↑ XMLGetAttrValues.js v1.6

--- VladSh/XMLGetAttrValues.js
+++ VladSh/XMLGetAttrValues.js
@@ -1,12 +1,15 @@
 ///Receiving appointed attributes of each line of XML-file
 ///Вытягивание определённых атрибутов из каждой строки файла с XML-структурой
 // http://akelpad.sourceforge.net/forum/viewtopic.php?p=11340#11340
-// Version: 1.5 (2012.09.10)
+// Version: 1.6 (2012.12.18)
+// 
+// Параметры:
+// 	sep - разделитель для разбора идентификаторов и надписей в param-файле (подробнее см. параметр pParsepProcSep в ShowMenuEx.js)
 
 if (! AkelPad.Include("ShowMenuEx.js")) WScript.Quit();
 if (! AkelPad.Include("ProcessRowText.js")) WScript.Quit();
 
-var arrAttr = getSelectedMenuItem(POS_CURSOR, "0", 0).split(", ");
+var arrAttr = getSelectedMenuItem(POS_CURSOR, AkelPad.GetArgValue("sep", ""), 0).split(", ");
 
 //создаём структуру искомых тэгов, перечень которых берём из файла параметров
 var structAttr = {};
@@ -22,8 +25,7 @@ AkelPad.Call("Log::Output", 5, pContent + "\r", -1, 1);
 
 
 //функция обработки отдельной строки
-function processString(rowXML)
-{
+function processString(rowXML) {
 	var pResult = "";
 	var aRES = getAttributesValues(rowXML);
 	if (aRES != null)
@@ -31,22 +33,19 @@ function processString(rowXML)
 	return pResult
 }
 
-function getAttributesValues(sXML)
-{
+function getAttributesValues(sXML) {
 	var aXML = sXML.split('"');
 	
 	var sATTR = "";
 	var aRES = [];
 	var r = 0;
 	
-	for (var n = 0; n <= aXML.length; n++)
-	{
+	for (var n = 0; n <= aXML.length; n++) {
 		sATTR = aXML[n].replace(/ /g, "");
 		sATTR = sATTR.replace(/=/g, "");
 		sATTR = sATTR.replace(/</g, "");
 		
-		if (structAttr[sATTR] == 1)
-		{
+		if (structAttr[sATTR] == 1) {
 			aRES[r] = aXML[n + 1];
 			r += 1;
 		}
@@ -56,7 +55,6 @@ function getAttributesValues(sXML)
 	if (r > 0) return aRES
 }
 
-function addToResult(arrOutput, vResult)
-{
+function addToResult(arrOutput, vResult) {
 	if (vResult) arrOutput[arrOutput.length] = vResult;
 }
