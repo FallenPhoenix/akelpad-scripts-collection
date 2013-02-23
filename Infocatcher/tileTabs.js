c77493c169b32ae1346daa422072cff774f21a64
F. Phoenix <fphoenix@email.com>
2013-02-24 00:23:00 +0400

    ↑ tileTabs.js v0.1.2

--- Infocatcher/tileTabs.js
+++ Infocatcher/tileTabs.js
@@ -1,16 +1,19 @@
 ﻿// http://akelpad.sourceforge.net/forum/viewtopic.php?p=17271#17271
 // http://infocatcher.ucoz.net/js/akelpad_scripts/tileTabs.js
+// https://github.com/Infocatcher/AkelPad_scripts/blob/master/tileTabs.js
 
-// (c) Infocatcher 2012
-// version 0.1.1 - 2012-03-18
+// (c) Infocatcher 2012-2013
+// version 0.1.2 - 2013-02-24
 
 // Tile current tab with next selected:
 // select first tab, call script and then select second tab.
 // Required MDI window mode!
 
 // Usage:
-//   Call("Scripts::Main", 1, "tileTabs.js")       - tile vertical
-//   Call("Scripts::Main", 1, "tileTabs.js", "h")  - tile horizontal
+//   Call("Scripts::Main", 1, "tileTabs.js")         - tile vertical
+//   Call("Scripts::Main", 1, "tileTabs.js", "o")    - tile vertical and preserve tabs order
+//   Call("Scripts::Main", 1, "tileTabs.js", "h")    - tile horizontal
+//   Call("Scripts::Main", 1, "tileTabs.js", "h o")  - tile horizontal and preserve tabs order
 
 function _localize(s) {
 	var strings = {
@@ -44,19 +47,58 @@ if(
 ) {
 	var hMdiClient = AkelPad.SendMessage(hMainWnd, 1222 /*AKD_GETMAININFO*/, 12 /*MI_WNDMDICLIENT*/, 0);
 	var lpFrame = AkelPad.SendMessage(hMainWnd, 1288 /*AKD_FRAMEFIND*/, 1 /*FWF_CURRENT*/, 0);
-	if(hMdiClient && lpFrame && AkelPad.WindowSubClass(hMainWnd, mainCallback, 0x416 /*AKDN_FRAME_ACTIVATE*/)) {
+	var lpFrame2;
+
+	if(hMdiClient && lpFrame) {
 		var statusbar = new Statusbar();
 		statusbar.save();
-		statusbar.set(_localize("Select tab!"));
+		var statusMsg = _localize("Select tab!");
+		statusbar.set(statusMsg);
 
-		var tileHorizontal = WScript.Arguments.length && WScript.Arguments(0) == "h";
+		var showDelay = 600;
+		var hideDelay = 150;
+		try {
+			var window = new ActiveXObject("htmlfile").parentWindow;
+			var shown = true;
+			var timer = window.setTimeout(function blink() {
+				statusbar.set(shown ? "" : statusMsg);
+				timer = window.setTimeout(blink, (shown = !shown) ? showDelay : hideDelay);
+			}, showDelay);
+		}
+		catch(e) {
+		}
+	}
 
+	if(
+		hMdiClient && lpFrame
+		&& AkelPad.WindowSubClass(
+			hMainWnd,
+			mainCallback,
+			0x416 /*AKDN_FRAME_ACTIVATE*/,
+			0x418 /*AKDN_FRAME_DESTROY*/
+		)
+	) {
+		AkelPad.ScriptNoMutex(5 /*ULT_UNLOCKSCRIPTSQUEUE|ULT_LOCKMULTICOPY*/); // Allow other scripts running
 		AkelPad.WindowGetMessage(); // Message loop
 		AkelPad.WindowUnsubClass(hMainWnd);
 
+		timer && window.clearTimeout(timer);
+		statusbar.restore();
+		if(lpFrame2) {
+			var tileHorizontal, useTabsOrder;
+			for(var i = 0, l = WScript.Arguments.length; i < l; ++i) {
+				switch(WScript.Arguments(i)) {
+					case "h": tileHorizontal = true; break;
+					case "o": useTabsOrder = true;
+				}
+			}
+			tileTabs(lpFrame, lpFrame2, tileHorizontal, useTabsOrder);
+		}
 		//AkelPad.SendMessage(hMainWnd, 1285 /*AKD_FRAMEACTIVATE*/, 0, lpFrame);
 	}
 	else {
+		timer && window.clearTimeout(timer);
+		statusbar && statusbar.restore();
 		AkelPad.MessageBox(hMainWnd, _localize("No tabs!"), WScript.ScriptName, 48 /*MB_ICONEXCLAMATION*/);
 	}
 }
@@ -64,18 +106,19 @@ else {
 	AkelPad.MessageBox(hMainWnd, _localize("MDI window mode required!"), WScript.ScriptName, 48 /*MB_ICONEXCLAMATION*/);
 }
 function mainCallback(hWnd, uMsg, wParam, lParam) {
-	if(uMsg != 0x416 /*AKDN_FRAME_ACTIVATE*/)
-		return;
-	var lpFrame2 = lParam;
-	if(lpFrame2 == lpFrame)
-		return;
-	oSys.Call("user32::PostQuitMessage", 0); // Exit message loop
-	tileTabs(lpFrame, lpFrame2, tileHorizontal);
+	if(uMsg == 0x416 /*AKDN_FRAME_ACTIVATE*/) {
+		if(lParam != lpFrame) {
+			lpFrame2 = lParam;
+			oSys.Call("user32::PostQuitMessage", 0); // Exit message loop
+		}
+	}
+	else if(uMsg == 0x418 /*AKDN_FRAME_DESTROY*/) {
+		if(lParam == lpFrame)
+			oSys.Call("user32::PostQuitMessage", 0); // Exit message loop
+	}
 }
 
-function tileTabs(lpFrame, lpFrame2, tileHorizontal) {
-	statusbar.restore();
-
+function tileTabs(lpFrame, lpFrame2, tileHorizontal, useTabsOrder) {
 	var lpRect = AkelPad.MemAlloc(16 /*sizeof(RECT)*/);
 	if(!lpRect)
 		return;
@@ -86,8 +129,22 @@ function tileTabs(lpFrame, lpFrame2, tileHorizontal) {
 	var hWndMdi  = AkelPad.SendMessage(hMainWnd, 1223 /*AKD_GETFRAMEINFO*/, 1 /*FI_WNDEDITPARENT*/, lpFrame);
 	var hWndMdi2 = AkelPad.SendMessage(hMainWnd, 1223 /*AKD_GETFRAMEINFO*/, 1 /*FI_WNDEDITPARENT*/, lpFrame2);
 
+	AkelPad.SendMessage(hMdiClient, 0x0223 /*WM_MDIRESTORE*/, hWndMdi2, 0);
+	AkelPad.SendMessage(hMdiClient, 0x0223 /*WM_MDIRESTORE*/, hWndMdi, 0);
+	AkelPad.SendMessage(hMainWnd, 1285 /*AKD_FRAMEACTIVATE*/, 0, lpFrame2);
+
 	var w = rcClient.right - rcClient.left;
 	var h = rcClient.bottom - rcClient.top;
+
+	if(useTabsOrder) {
+		var pos  = AkelPad.SendMessage(hMainWnd, 1294 /*AKD_FRAMEINDEX*/, 0, lpFrame);
+		var pos2 = AkelPad.SendMessage(hMainWnd, 1294 /*AKD_FRAMEINDEX*/, 0, lpFrame2);
+		if(pos2 < pos) {
+			var tmp = hWndMdi;
+			hWndMdi = hWndMdi2;
+			hWndMdi2 = tmp;
+		}
+	}
 
 	if(tileHorizontal) {
 		h /= 2;
@@ -101,8 +158,6 @@ function tileTabs(lpFrame, lpFrame2, tileHorizontal) {
 	}
 }
 function moveMdiWindow(hWndMdi, x, y, w, h) {
-	if(oSys.Call("user32::GetWindowLong" + _TCHAR, hWndMdi, -16 /*GWL_STYLE*/) & 0x001000000 /*WS_MAXIMIZE*/)
-		AkelPad.SendMessage(hMdiClient, 0x0223 /*WM_MDIRESTORE*/, hWndMdi, 0);
 	oSys.Call("user32::MoveWindow", hWndMdi, x, y, w, h, true /*bRepaint*/);
 }
 function parseRect(lpRect) {
@@ -133,6 +188,7 @@ function Statusbar() {
 			AkelPad.MemFree(lpTextBuffer);
 			return ret;
 		}
+		return undefined;
 	}
 	this.get = function() {
 		return buffer(function(lpTextBuffer) {
@@ -151,7 +207,7 @@ function Statusbar() {
 		_origStatus = this.get();
 	};
 	this.restore = function() {
-		if(_origStatus && this.get() == _customStatus)
+		if(_origStatus != undefined && this.get() == _customStatus)
 			this.set(_origStatus);
 	};
 }
