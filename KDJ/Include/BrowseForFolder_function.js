c012ba61b912f567c78ec1a6e70bc0b3a509e4d0
F. Phoenix <fphoenix@email.com>
2013-08-07 20:06:00 +0400

    ↑ BrowseForFolder_function.js <2013-08-07>

--- KDJ/Include/BrowseForFolder_function.js
+++ KDJ/Include/BrowseForFolder_function.js
@@ -1,49 +1,47 @@
-// BrowseForFolder_function.js - ver. 2013-04-07
+// BrowseForFolder_function.js - ver. 2013-08-07 (x86/x64)
 //
 // Call in script:
 // if (! AkelPad.Include("BrowseForFolder_function.js")) WScript.Quit();
-// sDir = BrowseForFolder(hWndOwn, sTitle, sIniDir, sDlgTitle);
+// sDir = BrowseForFolder(hWndOwn, sText, sIniDir, sTitle);
 //
 // All arguments are optional:
-// hWndOwn   - handle to the owner window for the dialog box, default is desktop window
-// sTitle    - string that is displayed above the tree view control in the dialog box, default is ""
-// sIniDir   - folder to be selected, after calling the function, default is ""
-// sDlgTitle - dialog box title, default is standard title
+// hWndOwn - handle to the owner window for the dialog box, default is desktop window
+// sText   - text that is displayed above the tree view control in the dialog box, default is ""
+// sIniDir - folder to be selected, after calling the function, default is ""
+// sTitle  - dialog box title, default is standard title
 //
 // Function returns string - selected folder name or "" if you press Cancel button
 
-function BrowseForFolder(hWndOwn, sTitle, sIniDir, sDlgTitle)
+function BrowseForFolder(hWndOwn, sText, sIniDir, sTitle)
 {
-  var DT_UNICODE = 1;
-  var DT_DWORD   = 3;
-  var oSys       = AkelPad.SystemFunction();
-  var hWndDesk   = oSys.Call("User32::GetDesktopWindow");
+  var oSys     = AkelPad.SystemFunction();
+  var hWndDesk = oSys.Call("User32::GetDesktopWindow");
 
   if (! oSys.Call("User32::IsWindow", hWndOwn))
     hWndOwn = hWndDesk;
-  if (typeof sTitle != "string")
-    sTitle = "";
+  if (typeof sText != "string")
+    sText = "";
   if (typeof sIniDir != "string")
     sIniDir = "";
 
   var lpCallback = oSys.RegisterCallback("", BFFCallback, 4);
-  var lpTitle    = AkelPad.MemAlloc((sTitle.length + 1) * 2);
+  var lpText     = AkelPad.MemAlloc((sText.length + 1) * 2);
   var lpDir      = AkelPad.MemAlloc(260 * 2); //sizeof(MAX_PATH)
-  var lpBrowse   = AkelPad.MemAlloc(32); //sizeof(BROWSEINFO)
+  var lpBrowse   = AkelPad.MemAlloc(_X64 ? 64 : 32); //sizeof(BROWSEINFO)
   var sDir       = "";
   var lpIDL;
 
-  AkelPad.MemCopy(lpTitle, sTitle, DT_UNICODE);
-  AkelPad.MemCopy(lpDir,  sIniDir, DT_UNICODE);
+  AkelPad.MemCopy(lpText, sText, 1 /*DT_UNICODE*/);
+  AkelPad.MemCopy(lpDir, sIniDir, 1 /*DT_UNICODE*/);
 
-  AkelPad.MemCopy(lpBrowse     ,    hWndOwn, DT_DWORD); //hWndOwner
-  AkelPad.MemCopy(lpBrowse +  4,          0, DT_DWORD); //pidlRoot
-  AkelPad.MemCopy(lpBrowse +  8,      lpDir, DT_DWORD); //pszDisplayName
-  AkelPad.MemCopy(lpBrowse + 12,    lpTitle, DT_DWORD); //lpszTitle
-  AkelPad.MemCopy(lpBrowse + 16, 0x00000041, DT_DWORD); //ulFlags = BIF_NEWDIALOGSTYLE|BIF_RETURNONLYFSDIRS
-  AkelPad.MemCopy(lpBrowse + 20, lpCallback, DT_DWORD); //lpfn
-  AkelPad.MemCopy(lpBrowse + 24,          0, DT_DWORD); //lParam = lpData in callback function
-  AkelPad.MemCopy(lpBrowse + 28,          0, DT_DWORD); //iImage
+  AkelPad.MemCopy(lpBrowse,                       hWndOwn, 2 /*DT_QWORD*/); //hWndOwner
+  AkelPad.MemCopy(lpBrowse + (_X64 ?  8 :  4),          0, 2 /*DT_QWORD*/); //pidlRoot
+  AkelPad.MemCopy(lpBrowse + (_X64 ? 16 :  8),      lpDir, 2 /*DT_QWORD*/); //pszDisplayName
+  AkelPad.MemCopy(lpBrowse + (_X64 ? 24 : 12),     lpText, 2 /*DT_QWORD*/); //lpszTitle
+  AkelPad.MemCopy(lpBrowse + (_X64 ? 32 : 16), 0x00000041, 3 /*DT_DWORD*/); //ulFlags = BIF_NEWDIALOGSTYLE|BIF_RETURNONLYFSDIRS
+  AkelPad.MemCopy(lpBrowse + (_X64 ? 40 : 20), lpCallback, 2 /*DT_QWORD*/); //lpfn
+  AkelPad.MemCopy(lpBrowse + (_X64 ? 48 : 24),          0, 2 /*DT_QWORD*/); //lParam = lpData in callback function
+  AkelPad.MemCopy(lpBrowse + (_X64 ? 56 : 28),          0, 3 /*DT_DWORD*/); //iImage
 
   oSys.Call("Ole32::CoInitialize", 0);
   lpIDL = oSys.Call("Shell32::SHBrowseForFolderW", lpBrowse);
@@ -51,15 +49,16 @@ function BrowseForFolder(hWndOwn, sTitle, sIniDir, sDlgTitle)
   if (lpIDL)
   {
     oSys.Call("Shell32::SHGetPathFromIDListW", lpIDL, lpDir);
-    sDir = AkelPad.MemRead(lpDir, DT_UNICODE);
+    sDir = AkelPad.MemRead(lpDir, 1 /*DT_UNICODE*/);
   }
 
   oSys.Call("Ole32::CoTaskMemFree", lpIDL);
   oSys.Call("Ole32::CoUninitialize");
 
-  AkelPad.MemFree(lpBrowse);
-  AkelPad.MemFree(lpTitle);
   oSys.UnregisterCallback(lpCallback);
+  AkelPad.MemFree(lpText);
+  AkelPad.MemFree(lpDir);
+  AkelPad.MemFree(lpBrowse);
 
   return sDir;
 
@@ -74,34 +73,38 @@ function BrowseForFolder(hWndOwn, sTitle, sIniDir, sDlgTitle)
       var nOwnX, nOwnY, nOwnW, nOwnH;
       var nDeskW, nDeskH;
 
-      if (typeof sDlgTitle == "string")
-        oSys.Call("User32::SetWindowTextW", hWnd, sDlgTitle);
+      if (typeof sTitle == "string")
+        oSys.Call("User32::SetWindowTextW", hWnd, sTitle);
 
-      AkelPad.SendMessage(hWnd, 0x0467 /*BFFM_SETSELECTIONW*/, 1, lpDir);
+      oSys.Call("User32::SendMessageW", hWnd, 0x0467 /*BFFM_SETSELECTIONW*/, 1, lpDir);
 
       //set focus to TreeView
       if (hWnd1 = oSys.Call("User32::FindWindowExW", hWnd, 0, "SHBrowseForFolder ShellNameSpace Control", 0))
       {
         if (hWnd2 = oSys.Call("User32::FindWindowExW", hWnd1, 0, "SysTreeView32", 0))
+        {
+          WScript.Sleep(50);
           oSys.Call("User32::PostMessageW", hWnd, 0x0028 /*WM_NEXTDLGCTL*/, hWnd2, 1);
+          oSys.Call("User32::PostMessageW", hWnd2, 0x1114 /*TVM_ENSUREVISIBLE*/, 0, oSys.Call("User32::SendMessageW", hWnd2, 0x110A /*TVM_GETNEXTITEM*/, 0x0009 /*TVGN_CARET*/, 0));
+        }
       }
 
       //center dialog
       oSys.Call("User32::GetWindowRect", hWnd, lpRect);
-      nWndX = AkelPad.MemRead(lpRect,      DT_DWORD);
-      nWndY = AkelPad.MemRead(lpRect +  4, DT_DWORD);
-      nWndW = AkelPad.MemRead(lpRect +  8, DT_DWORD) - nWndX;
-      nWndH = AkelPad.MemRead(lpRect + 12, DT_DWORD) - nWndY;
+      nWndX = AkelPad.MemRead(lpRect,      3 /*DT_DWORD*/);
+      nWndY = AkelPad.MemRead(lpRect +  4, 3 /*DT_DWORD*/);
+      nWndW = AkelPad.MemRead(lpRect +  8, 3 /*DT_DWORD*/) - nWndX;
+      nWndH = AkelPad.MemRead(lpRect + 12, 3 /*DT_DWORD*/) - nWndY;
 
       oSys.Call("User32::GetWindowRect", hWndOwn, lpRect);
-      nOwnX = AkelPad.MemRead(lpRect,      DT_DWORD);
-      nOwnY = AkelPad.MemRead(lpRect +  4, DT_DWORD);
-      nOwnW = AkelPad.MemRead(lpRect +  8, DT_DWORD) - nOwnX;
-      nOwnH = AkelPad.MemRead(lpRect + 12, DT_DWORD) - nOwnY;
+      nOwnX = AkelPad.MemRead(lpRect,      3 /*DT_DWORD*/);
+      nOwnY = AkelPad.MemRead(lpRect +  4, 3 /*DT_DWORD*/);
+      nOwnW = AkelPad.MemRead(lpRect +  8, 3 /*DT_DWORD*/) - nOwnX;
+      nOwnH = AkelPad.MemRead(lpRect + 12, 3 /*DT_DWORD*/) - nOwnY;
 
       oSys.Call("User32::GetWindowRect", hWndDesk, lpRect);
-      nDeskW = AkelPad.MemRead(lpRect +  8, DT_DWORD);
-      nDeskH = AkelPad.MemRead(lpRect + 12, DT_DWORD);
+      nDeskW = AkelPad.MemRead(lpRect +  8, 3 /*DT_DWORD*/);
+      nDeskH = AkelPad.MemRead(lpRect + 12, 3 /*DT_DWORD*/);
       AkelPad.MemFree(lpRect);
 
       nWndX = nOwnX + (nOwnW - nWndW) / 2;
@@ -116,7 +119,7 @@ function BrowseForFolder(hWndOwn, sTitle, sIniDir, sDlgTitle)
       if (nWndY < 0)
         nWndY = 0;
 
-      oSys.Call("User32::MoveWindow", hWnd, nWndX, nWndY, nWndW, nWndH, 0);
+      oSys.Call("User32::SetWindowPos", hWnd, 0, nWndX, nWndY, 0, 0, 0x15 /*SWP_NOZORDER|SWP_NOACTIVATE|SWP_NOSIZE*/);
     }
 
     return 0;
