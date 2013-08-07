b0a471d918b8ac576d84f147270c6b4f5404a210
F. Phoenix <fphoenix@email.com>
2013-08-07 20:06:00 +0400

    ↑ InsertFolderName.js <2013-08-07>

--- KDJ/InsertFolderName.js
+++ KDJ/InsertFolderName.js
@@ -1,59 +1,118 @@
-// Inserts folder name using SHBrowseForFolder function - 2011-06-22
+// InsertFolderName.js - ver. 2013-08-07 (x86/x64)
 //
+// Inserts folder name using SHBrowseForFolder function.
+//
+// Usage:
 // Call("Scripts::Main", 1, "InsertFolderName.js")
 
-var hEditWnd = AkelPad.GetEditWnd();
-if (! hEditWnd)
-  WScript.Quit();
+var hWndOwn = AkelPad.GetEditWnd();
+if (! hWndOwn) WScript.Quit();
 
-var DT_DWORD   = 3;
 var oSys       = AkelPad.SystemFunction();
 var sFile      = AkelPad.GetEditFile(0);
-var sDir       = "";
-var lpCallback = oSys.RegisterCallback("BrowseForFolderCallback");
-var lpDir      = AkelPad.MemAlloc(260 * _TSIZE); //sizeof(MAX_PATH)
-var lpBrowse   = AkelPad.MemAlloc(32);           //sizeof(BROWSEINFO)
+var sDir       = (sFile) ? sFile.substr(0, sFile.lastIndexOf("\\") + 1) : "";
+var sText      = "Choose folder:";
+var lpCallback = oSys.RegisterCallback("BFFCallback");
+var lpText     = AkelPad.MemAlloc((sText.length + 1) * 2);
+var lpDir      = AkelPad.MemAlloc(260 * 2); //sizeof(MAX_PATH)
+var lpBrowse   = AkelPad.MemAlloc(_X64 ? 64 : 32); //sizeof(BROWSEINFO)
 var lpIDL;
 
-if (sFile)
-  sDir = sFile.substr(0, sFile.lastIndexOf("\\") + 1);
+AkelPad.MemCopy(lpText, sText, 1 /*DT_UNICODE*/);
+AkelPad.MemCopy(lpDir, sDir, 1 /*DT_UNICODE*/);
 
-AkelPad.MemCopy(lpDir, sDir, _TSTR);
-
-AkelPad.MemCopy(lpBrowse     ,   hEditWnd, DT_DWORD); //hwndOwner
-AkelPad.MemCopy(lpBrowse +  4,          0, DT_DWORD); //pidlRoot
-AkelPad.MemCopy(lpBrowse +  8,      lpDir, DT_DWORD); //pszDisplayName
-AkelPad.MemCopy(lpBrowse + 12,          0, DT_DWORD); //lpszTitle
-AkelPad.MemCopy(lpBrowse + 16, 0x00000041, DT_DWORD); //ulFlags = BIF_NEWDIALOGSTYLE|BIF_RETURNONLYFSDIRS
-AkelPad.MemCopy(lpBrowse + 20, lpCallback, DT_DWORD); //lpfn
-AkelPad.MemCopy(lpBrowse + 24,      lpDir, DT_DWORD); //lParam
-AkelPad.MemCopy(lpBrowse + 28,          0, DT_DWORD); //iImage
+AkelPad.MemCopy(lpBrowse,                       hWndOwn, 2 /*DT_QWORD*/); //hwndOwner
+AkelPad.MemCopy(lpBrowse + (_X64 ?  8 :  4),          0, 2 /*DT_QWORD*/); //pidlRoot
+AkelPad.MemCopy(lpBrowse + (_X64 ? 16 :  8),      lpDir, 2 /*DT_QWORD*/); //pszDisplayName
+AkelPad.MemCopy(lpBrowse + (_X64 ? 24 : 12),     lpText, 2 /*DT_QWORD*/); //lpszTitle
+AkelPad.MemCopy(lpBrowse + (_X64 ? 32 : 16), 0x00000041, 3 /*DT_DWORD*/); //ulFlags = BIF_NEWDIALOGSTYLE|BIF_RETURNONLYFSDIRS
+AkelPad.MemCopy(lpBrowse + (_X64 ? 40 : 20), lpCallback, 2 /*DT_QWORD*/); //lpfn
+AkelPad.MemCopy(lpBrowse + (_X64 ? 48 : 24),          0, 2 /*DT_QWORD*/); //lParam
+AkelPad.MemCopy(lpBrowse + (_X64 ? 56 : 28),          0, 3 /*DT_DWORD*/); //iImage
 
 oSys.Call("Ole32::CoInitialize", 0);
-lpIDL = oSys.Call("Shell32::SHBrowseForFolder" + _TCHAR, lpBrowse);
+lpIDL = oSys.Call("Shell32::SHBrowseForFolderW", lpBrowse);
 
 if (lpIDL)
 {
-  oSys.Call("Shell32::SHGetPathFromIDList" + _TCHAR, lpIDL, lpDir);
+  oSys.Call("Shell32::SHGetPathFromIDListW", lpIDL, lpDir);
 
-  sDir = AkelPad.MemRead(lpDir, _TSTR);
-  if (sDir && (! /\\$/.test(sDir)))
-    sDir += "\\";
+  sDir = AkelPad.MemRead(lpDir, 1 /*DT_UNICODE*/);
 
-  AkelPad.ReplaceSel(sDir, 1);
+  if (sDir)
+  {
+    if (! /\\$/.test(sDir)) sDir += "\\";
+    AkelPad.ReplaceSel(sDir, 1);
+  }
 }
 
 oSys.Call("Ole32::CoTaskMemFree, lpIDL");
 oSys.Call("Ole32::CoUninitialize");
 
-AkelPad.MemFree(lpBrowse);
-AkelPad.MemFree(lpDir);
 oSys.UnregisterCallback(lpCallback);
+AkelPad.MemFree(lpText);
+AkelPad.MemFree(lpDir);
+AkelPad.MemFree(lpBrowse);
 
-function BrowseForFolderCallback(hWnd, uMsg, lParam, lpData)
+function BFFCallback(hWnd, uMsg, lParam, lpData)
 {
   if (uMsg == 1 /*BFFM_INITIALIZED*/)
-    AkelPad.SendMessage(hWnd, 0x0467 /*BFFM_SETSELECTIONW*/, 1, lpData);
+  {
+    var hWndDesk = oSys.Call("User32::GetDesktopWindow");
+    var lpRect   = AkelPad.MemAlloc(16); //sizeof(RECT)
+    var hWnd1;
+    var hWnd2;
+    var nWndX, nWndY, nWndW, nWndH;
+    var nOwnX, nOwnY, nOwnW, nOwnH;
+    var nDeskW, nDeskH;
+
+    oSys.Call("User32::SetWindowTextW", hWnd, "Insert folder name");
+
+    oSys.Call("User32::SendMessageW", hWnd, 0x0467 /*BFFM_SETSELECTIONW*/, 1, lpDir);
+
+    //set focus to TreeView
+    if (hWnd1 = oSys.Call("User32::FindWindowExW", hWnd, 0, "SHBrowseForFolder ShellNameSpace Control", 0))
+    {
+      if (hWnd2 = oSys.Call("User32::FindWindowExW", hWnd1, 0, "SysTreeView32", 0))
+      {
+        WScript.Sleep(50);
+        oSys.Call("User32::PostMessageW", hWnd, 0x0028 /*WM_NEXTDLGCTL*/, hWnd2, 1);
+        oSys.Call("User32::PostMessageW", hWnd2, 0x1114 /*TVM_ENSUREVISIBLE*/, 0, oSys.Call("User32::SendMessageW", hWnd2, 0x110A /*TVM_GETNEXTITEM*/, 0x0009 /*TVGN_CARET*/, 0));
+      }
+    }
+
+    //center dialog
+    oSys.Call("User32::GetWindowRect", hWnd, lpRect);
+    nWndX = AkelPad.MemRead(lpRect,      3 /*DT_DWORD*/);
+    nWndY = AkelPad.MemRead(lpRect +  4, 3 /*DT_DWORD*/);
+    nWndW = AkelPad.MemRead(lpRect +  8, 3 /*DT_DWORD*/) - nWndX;
+    nWndH = AkelPad.MemRead(lpRect + 12, 3 /*DT_DWORD*/) - nWndY;
+
+    oSys.Call("User32::GetWindowRect", hWndOwn, lpRect);
+    nOwnX = AkelPad.MemRead(lpRect,      3 /*DT_DWORD*/);
+    nOwnY = AkelPad.MemRead(lpRect +  4, 3 /*DT_DWORD*/);
+    nOwnW = AkelPad.MemRead(lpRect +  8, 3 /*DT_DWORD*/) - nOwnX;
+    nOwnH = AkelPad.MemRead(lpRect + 12, 3 /*DT_DWORD*/) - nOwnY;
+
+    oSys.Call("User32::GetWindowRect", hWndDesk, lpRect);
+    nDeskW = AkelPad.MemRead(lpRect +  8, 3 /*DT_DWORD*/);
+    nDeskH = AkelPad.MemRead(lpRect + 12, 3 /*DT_DWORD*/);
+    AkelPad.MemFree(lpRect);
+
+    nWndX = nOwnX + (nOwnW - nWndW) / 2;
+    nWndY = nOwnY + (nOwnH - nWndH) / 2;
+
+    if ((nWndX + nWndW) > nDeskW)
+      nWndX = nDeskW - nWndW;
+    if (nWndX < 0)
+      nWndX = 0;
+    if ((nWndY + nWndH) > nDeskH)
+      nWndY = nDeskH - nWndH;
+    if (nWndY < 0)
+      nWndY = 0;
+
+    oSys.Call("User32::SetWindowPos", hWnd, 0, nWndX, nWndY, 0, 0, 0x15 /*SWP_NOZORDER|SWP_NOACTIVATE|SWP_NOSIZE*/);
+  }
 
   return 0;
 }
--- KDJ/InsertFolderName_alternative.js
+++ KDJ/InsertFolderName_alternative.js
@@ -1,20 +1,24 @@
-// Inserts folder name using SHBrowseForFolder function - 2011-06-22
+// InsertFolderName_alternative.js - ver. 2013-08-07 (x86/x64)
 //
-// Alternative version with AkelPad.Include("BrowseForFolder_function.js")
-// Script BrowseForFolder_function.js must be placed in directory ...\Scripts\Include
+// Inserts folder name using SHBrowseForFolder function.
+// Alternative version with AkelPad.Include("BrowseForFolder_function.js").
+// Script BrowseForFolder_function.js must be placed in directory ...\AkelPad\AkelFiles\Plugs\Scripts\Include\
+//
+// Usage:
 // Call("Scripts::Main", 1, "InsertFolderName_alternative.js")
 
 var hEditWnd = AkelPad.GetEditWnd();
 var sFile    = AkelPad.GetEditFile(0);
-var sTitle   = "Choose folder for inserting to text:";
+var sText    = "Choose folder:";
 var sDir     = "";
+var sTitle   = "Insert folder name";
 
 if ((hEditWnd) && AkelPad.Include("BrowseForFolder_function.js"))
 {
   if (sFile)
     sDir = sFile.substr(0, sFile.lastIndexOf("\\") + 1);
 
-  sDir = BrowseForFolder(hEditWnd, sTitle, sDir);
+  sDir = BrowseForFolder(hEditWnd, sText, sDir, sTitle);
 
   if (sDir)
     AkelPad.ReplaceSel(sDir, 1);
