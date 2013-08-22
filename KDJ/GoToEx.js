228745a63d30d450efb50df816d2b4541048ff6c
F. Phoenix <fphoenix@email.com>
2013-08-22 20:08:00 +0400

    ↑ GoToEx (2013-08-22)

--- KDJ/GoToEx.js
+++ KDJ/GoToEx.js
@@ -1,4 +1,4 @@
-// GoToEx.js - ver. 2012-12-26
+// GoToEx.js - ver. 2013-08-22 (x86/x64)
 //
 // "Go to" dialog with argument
 //
@@ -38,13 +38,13 @@ if (nLineArg >= 0)
 
     if (hWndLine && hWndOffset)
     {
-      AkelPad.SendMessage(hWndLine,   241 /*BM_SETCHECK*/, nLineArg,   0);
-      AkelPad.SendMessage(hWndOffset, 241 /*BM_SETCHECK*/, ! nLineArg, 0);
+      SendMessage(hWndLine,   241 /*BM_SETCHECK*/, nLineArg,   0);
+      SendMessage(hWndOffset, 241 /*BM_SETCHECK*/, ! nLineArg, 0);
 
       if (nLineArg)
-        AkelPad.SendMessage(hWndGoTo, 273 /*WM_COMMAND*/, IDC_GOTO_LINE, hWndLine);
+        SendMessage(hWndGoTo, 273 /*WM_COMMAND*/, IDC_GOTO_LINE, hWndLine);
       else
-        AkelPad.SendMessage(hWndGoTo, 273 /*WM_COMMAND*/, IDC_GOTO_OFFSET, hWndOffset);
+        SendMessage(hWndGoTo, 273 /*WM_COMMAND*/, IDC_GOTO_OFFSET, hWndOffset);
     }
   }
 }
@@ -52,7 +52,7 @@ if (nLineArg >= 0)
 function GetGoToDialog()
 {
   var lpMLT = AkelPad.MemAlloc(4);
-  var hWnd  = AkelPad.SendMessage(AkelPad.GetMainWnd(), 1275 /*AKD_GETMODELESS*/, 0, lpMLT);
+  var hWnd  = SendMessage(AkelPad.GetMainWnd(), 1275 /*AKD_GETMODELESS*/, 0, lpMLT);
   var hDlg  = 0;
 
   if (AkelPad.MemRead(lpMLT, 3 /*DT_DWORD*/) == 5 /*MLT_GOTO*/)
@@ -61,4 +61,9 @@ function GetGoToDialog()
   AkelPad.MemFree(lpMLT);
 
   return hDlg;
+}
+
+function SendMessage(hWnd, uMsg, wParam, lParam)
+{
+  return oSys.Call("User32::SendMessageW", hWnd, uMsg, wParam, lParam);
 }
