c548daeb76017010b74aff4e927889d408ed3680
F. Phoenix <fphoenix@email.com>
2013-08-24 21:50:00 +0400

    ↑ ColumnsDuplicateSelect.js <2013-08-24>

--- KDJ/ColumnsDuplicateSelect.js
+++ KDJ/ColumnsDuplicateSelect.js
@@ -1,37 +1,14 @@
-// Select entire columns or duplicate selected columns - 2010-10-13
+// ColumnsDuplicateSelect.js - var. 2013-08-24 (x86/x64)
 //
-// Call("Scripts::Main", 1, "ColumnsDuplicateSelect.js", "0")  - select entire columns
-// Call("Scripts::Main", 1, "ColumnsDuplicateSelect.js", "2")  - duplicate columns
+// Select entire columns or duplicate selected columns.
+//
+// Usage:
+// Call("Scripts::Main", 1, "ColumnsDuplicateSelect.js"[, "Action"])
+// Action:
+//   0 - select entire columns (default)
+//   2 - duplicate columns
 //
 // Can assign shortcut keys, eg: Ctrl+Shift+C, Ctrl+Shift+D
-
-var DT_DWORD          = 3;
-var AEGL_LINECOUNT    = 0;
-var AEGI_FIRSTSELCHAR = 3;
-var AEGI_LASTSELCHAR  = 4;
-var AESELT_COLUMNON   = 0x1;
-var AEM_GETSEL        = 3125;
-var AEM_SETSEL        = 3126;
-var AEM_GETLINENUMBER = 3129;
-var AEM_GETINDEX      = 3130;
-var AEM_INDEXUPDATE   = 3132;
-var AEM_GETWORDWRAP   = 3241;
-var IDM_VIEW_WORDWRAP = 4209;
-
-var nWordWrap;
-var lpFirstC;
-var lpLastC;
-var lpCaret;
-var lpSelect;
-var lpBegSel;
-var lpEndSel;
-var lpSelFlag;
-var lpSelect1;
-var lpBegSel1;
-var lpEndSel1;
-var lpSelFlag1;
-var nLastLine;
-var pSelTxt;
 
 var hEditWnd = AkelPad.GetEditWnd();
 var nAction;
@@ -40,68 +17,70 @@ if (! hEditWnd)
   WScript.Quit();
 if (WScript.Arguments.length)
   nAction = WScript.Arguments(0);
-if (! ((nAction == 0) || (nAction == 2)))
-  WScript.Quit();
+if (nAction != 2)
+  nAction = 0;
 
+var DT_QWORD             = 2;
+var DT_DWORD             = 3;
+var AEGI_FIRSTSELCHAR    = 3;
+var AEGI_LASTSELCHAR     = 4;
+var AEGL_LINEUNWRAPCOUNT = 11;
+var AESELT_COLUMNON      = 0x1;
+var AEM_GETSEL           = 3125;
+var AEM_SETSEL           = 3126;
+var AEM_GETLINENUMBER    = 3129;
+var AEM_GETINDEX         = 3130;
+var AEM_INDEXUPDATE      = 3132;
+var AEM_GETWORDWRAP      = 3241;
+var IDM_VIEW_WORDWRAP    = 4209;
 
-lpFirstC  = AkelPad.MemAlloc(12 /*sizeof(AECHARINDEX)*/);
-lpLastC   = AkelPad.MemAlloc(12 /*sizeof(AECHARINDEX)*/);
-lpCaret   = AkelPad.MemAlloc(12 /*sizeof(AECHARINDEX)*/);
-lpSelect  = AkelPad.MemAlloc(28 /*sizeof(AESELECTION)*/);
-lpSelect1 = AkelPad.MemAlloc(28 /*sizeof(AESELECTION)*/);
+var oSys       = AkelPad.SystemFunction();
+var nWordWrap  = SendMessage(hEditWnd, AEM_GETWORDWRAP, 0, 0);
+var lpFirstC   = AkelPad.MemAlloc(_X64 ? 24 : 12 /*sizeof(AECHARINDEX)*/);
+var lpLastC    = AkelPad.MemAlloc(_X64 ? 24 : 12 /*sizeof(AECHARINDEX)*/);
+var lpCaret    = AkelPad.MemAlloc(_X64 ? 24 : 12 /*sizeof(AECHARINDEX)*/);
+var lpSelect   = AkelPad.MemAlloc(_X64 ? 56 : 28 /*sizeof(AESELECTION)*/);
+var lpSelect1  = AkelPad.MemAlloc(_X64 ? 56 : 28 /*sizeof(AESELECTION)*/);
+var lpBegSel   = lpSelect;
+var lpEndSel   = lpSelect + (_X64 ? 24 : 12);
+var lpSelFlag  = lpSelect + (_X64 ? 48 : 24);
+var lpBegSel1  = lpSelect1;
+var lpEndSel1  = lpSelect1 + (_X64 ? 24 : 12);
+var lpSelFlag1 = lpSelect1 + (_X64 ? 48 : 24);
+var nLastLine  = SendMessage(hEditWnd, AEM_GETLINENUMBER, AEGL_LINEUNWRAPCOUNT, 0) - 1;
+var sSelTxt;
 
-if ((! lpFirstC) || (! lpLastC) || (! lpCaret) || (! lpSelect) || (! lpSelect1))
-{
-  AkelPad.MemFree(lpFirstC);
-  AkelPad.MemFree(lpLastC);
-  AkelPad.MemFree(lpCaret);
-  AkelPad.MemFree(lpSelect);
-  AkelPad.MemFree(lpSelect1);
-  WScript.Quit();
-}
-
-// Word Wrap
-nWordWrap = AkelPad.SendMessage(hEditWnd, AEM_GETWORDWRAP, 0, 0);
-if (nWordWrap > 0)
+if (nWordWrap)
   AkelPad.Command(IDM_VIEW_WORDWRAP);
 
-AkelPad.SendMessage(hEditWnd, AEM_GETINDEX, AEGI_FIRSTSELCHAR, lpFirstC);
-AkelPad.SendMessage(hEditWnd, AEM_GETINDEX, AEGI_LASTSELCHAR,  lpLastC);
-AkelPad.SendMessage(hEditWnd, AEM_GETSEL, lpCaret, lpSelect);
+SendMessage(hEditWnd, AEM_GETINDEX, AEGI_FIRSTSELCHAR, lpFirstC);
+SendMessage(hEditWnd, AEM_GETINDEX, AEGI_LASTSELCHAR,  lpLastC);
+SendMessage(hEditWnd, AEM_GETSEL, lpCaret, lpSelect);
 
-lpBegSel  = lpSelect;
-lpEndSel  = lpSelect + 12;
-lpSelFlag = lpSelect + 24;
-nLastLine = AkelPad.SendMessage(hEditWnd, AEM_GETLINENUMBER, AEGL_LINECOUNT, 0) - 1;
-
-ReplaceCharIndex(lpBegSel, nLastLine, AkelPad.MemRead(lpFirstC + 8, DT_DWORD));
-ReplaceCharIndex(lpEndSel, 0,         AkelPad.MemRead(lpLastC  + 8, DT_DWORD));
+ReplaceCharIndex(lpBegSel, nLastLine, AkelPad.MemRead(lpFirstC + (_X64 ? 16 : 8), DT_DWORD));
+ReplaceCharIndex(lpEndSel, 0,         AkelPad.MemRead(lpLastC  + (_X64 ? 16 : 8), DT_DWORD));
 CopyCharIndex(lpCaret, lpEndSel);
 
 AkelPad.MemCopy(lpSelFlag, AESELT_COLUMNON, DT_DWORD);
 
-AkelPad.SendMessage(hEditWnd, AEM_SETSEL, lpCaret, lpSelect);
-
+SendMessage(hEditWnd, AEM_SETSEL, lpCaret, lpSelect);
 
 if (nAction == 2)
 {
-  pSelTxt    = AkelPad.GetSelText(1 /*\r*/);
-  lpBegSel1  = lpSelect1;
-  lpEndSel1  = lpSelect1 + 12;
-  lpSelFlag1 = lpSelect1 + 24;
+  sSelTxt = AkelPad.GetSelText(1 /*\r*/);
 
-  ReplaceCharIndex(lpBegSel1, 0, AkelPad.MemRead(lpLastC + 8, DT_DWORD));
-  ReplaceCharIndex(lpEndSel1, 0, AkelPad.MemRead(lpLastC + 8, DT_DWORD));
+  ReplaceCharIndex(lpBegSel1, 0, AkelPad.MemRead(lpLastC + (_X64 ? 16 : 8), DT_DWORD));
+  ReplaceCharIndex(lpEndSel1, 0, AkelPad.MemRead(lpLastC + (_X64 ? 16 : 8), DT_DWORD));
   AkelPad.MemCopy(lpSelFlag1, AESELT_COLUMNON, DT_DWORD);
 
   SetRedraw(hEditWnd, false);
-  AkelPad.SendMessage(hEditWnd, AEM_SETSEL, 0, lpSelect1);
-  AkelPad.ReplaceSel(pSelTxt);
+  SendMessage(hEditWnd, AEM_SETSEL, 0, lpSelect1);
+  AkelPad.ReplaceSel(sSelTxt);
 
-  AkelPad.SendMessage(hEditWnd, AEM_INDEXUPDATE, 0, lpBegSel);
-  AkelPad.SendMessage(hEditWnd, AEM_INDEXUPDATE, 0, lpEndSel);
-  AkelPad.SendMessage(hEditWnd, AEM_INDEXUPDATE, 0, lpCaret);
-  AkelPad.SendMessage(hEditWnd, AEM_SETSEL, lpCaret, lpSelect);
+  SendMessage(hEditWnd, AEM_INDEXUPDATE, 0, lpBegSel);
+  SendMessage(hEditWnd, AEM_INDEXUPDATE, 0, lpEndSel);
+  SendMessage(hEditWnd, AEM_INDEXUPDATE, 0, lpCaret);
+  SendMessage(hEditWnd, AEM_SETSEL, lpCaret, lpSelect);
   SetRedraw(hEditWnd, true);
 }
 
@@ -111,33 +90,33 @@ AkelPad.MemFree(lpCaret);
 AkelPad.MemFree(lpSelect);
 AkelPad.MemFree(lpSelect1);
 
-if (nWordWrap > 0)
+if (nWordWrap)
   AkelPad.Command(IDM_VIEW_WORDWRAP);
 
-
-////////////////////
 function SetRedraw(hWnd, bRedraw)
 {
-  var oSys = AkelPad.SystemFunction();
-  AkelPad.SendMessage(hWnd, 11 /*WM_SETREDRAW*/, bRedraw, 0);
-  bRedraw && oSys.Call("user32::InvalidateRect", hWnd, 0, true);
+  SendMessage(hWnd, 11 /*WM_SETREDRAW*/, bRedraw, 0);
+  bRedraw && oSys.Call("User32::InvalidateRect", hWnd, 0, true);
 }
-
 
 function CopyCharIndex(lpToIndex, lpFromIndex)
 {
-  AkelPad.MemCopy(lpToIndex,     AkelPad.MemRead(lpFromIndex,     DT_DWORD), DT_DWORD);
-  AkelPad.MemCopy(lpToIndex + 4, AkelPad.MemRead(lpFromIndex + 4, DT_DWORD), DT_DWORD);
-  AkelPad.MemCopy(lpToIndex + 8, AkelPad.MemRead(lpFromIndex + 8, DT_DWORD), DT_DWORD);
+  AkelPad.MemCopy(lpToIndex, AkelPad.MemRead(lpFromIndex, DT_DWORD), DT_DWORD);
+  AkelPad.MemCopy(lpToIndex + (_X64 ?  8 : 4), AkelPad.MemRead(lpFromIndex + (_X64 ?  8 : 4), DT_QWORD), DT_QWORD);
+  AkelPad.MemCopy(lpToIndex + (_X64 ? 16 : 8), AkelPad.MemRead(lpFromIndex + (_X64 ? 16 : 8), DT_DWORD), DT_DWORD);
 }
 
-
-function ReplaceCharIndex(lpIndex, nRow, nCol)  /*if nRow or nCol == -1, no change*/
+function ReplaceCharIndex(lpIndex, nRow, nCol) /*if nRow or nCol == -1, no change*/
 {
   if (nRow > -1)
-    AkelPad.MemCopy(lpIndex,     nRow, DT_DWORD);
+    AkelPad.MemCopy(lpIndex, nRow, DT_DWORD);
   if (nCol > -1)
-    AkelPad.MemCopy(lpIndex + 8, nCol, DT_DWORD);
+    AkelPad.MemCopy(lpIndex + (_X64 ? 16 : 8), nCol, DT_DWORD);
 
-  AkelPad.SendMessage(hEditWnd, AEM_INDEXUPDATE, 0, lpIndex);
+  SendMessage(hEditWnd, AEM_INDEXUPDATE, 0, lpIndex);
+}
+
+function SendMessage(hWnd, uMsg, wParam, lParam)
+{
+  return oSys.Call("User32::SendMessage" + _TCHAR, hWnd, uMsg, wParam, lParam);
 }
